a69e101ed47000796adba0bab08a582cc20b2d8f
asmith97 <alexsmith@wireless-s0-no-150-2-21.uchicago.edu>
2016-03-16 12:17:47 -0500

    making good progress
    still has the bug where it sometimes back tracks too far

--- AStar.hs
+++ AStar.hs
@@ -1,9 +1,59 @@
 import System.IO
 import Data.List
+import System.Environment
 
 data Tree a = Root (a,a) Int | Node (Tree a) (a,a) Int Int Int deriving (Show, Eq)
 type Board = [String]
 type Position = (Int, Int)
+
+--this has a couple of bugs
+--for example, it can successfully solve bigboard when the goal is (3,35)
+--but not when the goal is (1,35) it makes a few successful backtracks while going to
+--(3,35) but once it gets to (3,35) it jumps back to (2,29) which is too far for it to jump back to,
+--so then it cannot solve it.  With slight modifications it should be able to not jump back too far
+
+
+main :: IO ()
+main = do
+    args <- getArgs
+    board <- readFile (args !! 0)
+    let boardArray = lines board
+    putStr "Input the starting coordinates (x,y): "
+    hFlush stdout
+    startCoord <- getLine
+    putStr "Input the goal (x,y): "
+    hFlush stdout
+    endCoord <- getLine
+    let posTuple = (read startCoord :: (Int, Int))
+    let validPos = check posTuple boardArray
+    let endTuple = (read endCoord :: (Int, Int))
+    let validEndPos = check endTuple boardArray
+    if and $ fmap not [validPos, validEndPos] then
+            putStrLn "The inputted coordinates are not valid on the boards or are in a wall"
+    else
+        do
+            let path = findPath (Root posTuple 0) boardArray endTuple
+            mapM_ putStrLn boardArray
+            putStrLn $ printSolution (toArray path)
+
+toArray :: Tree Int -> [Position]
+toArray (Root (0,0) 0) = []
+toArray (Root t 0) = [t]
+toArray (Node parent tuple _ _ _) = (toArray parent) ++ [tuple]
+
+printSolution :: [Position] -> String
+printSolution [] = "No solution given this initial value"
+printSolution a = "Has length " ++ (show $ length a) ++ "\n" ++ show a
+
+check :: Position -> Board -> Bool
+check _ [] = True
+check pos@(x,y) board
+    | x < xMax && y < yMax && ((board !! y) !! x) /= '%' = True
+    | otherwise = False
+        where
+            yMax = length board
+            xMax = length $ board !! 0
+
 
 b = ["%%%%%","%___%","%-%-%","%%%-%","%---%","%%%%%"]
 
@@ -182,4 +232,42 @@ search start@(Node parent (x,y) g h skip) board goal [] = if (x,y) == goal then 
         sortedList = sortTreeList $ validTrees start board goal
 
 
+
+bigboard = ["%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    , "%-------%-%-%-----------%---%-----%-%"
+    , "%-%%%%%%%-%-%%%-%-%%%-%%%-%%%%%%%-%-%"
+    , "%-------%-------%-%-----%-----%-%---%"
+    , "%%%%%-%%%%%-%%%-%-%-%-%%%-%%%%%-%-%%%"
+    , "%---%-%-%-%---%-%-%-%---%-%---%-%---%"
+    , "%-%%%-%-%-%-%%%-%%%%%-%%%-%-%%%-%%%-%"
+    , "%-------%-----%---%---%-----%-%-%---%"
+    , "%%%-%%%%%%%%%-%%%%%%%-%%%-%%%-%-%-%-%"
+    , "%-------------%-------%-%---%-----%-%"
+    , "%-%-%%%%%-%-%%%-%-%-%%%-%-%%%-%%%-%-%"
+    , "%-%-%-----%-%-%-%-%-----%---%-%-%-%-%"
+    , "%-%-%-%%%%%%%-%-%%%%%%%%%-%%%-%-%%%-%"
+    , "%-%-%-%-----%---%-----%-----%---%---%"
+    , "%%%-%%%-%-%%%%%-%%%%%-%%%-%%%-%%%%%-%"
+    , "%-----%-%-%-----%-%-----%-%---%-%-%-%"
+    , "%-%-%-%-%-%%%-%%%-%%%-%%%-%-%-%-%-%-%"
+    , "%-%-%-%-%-----------------%-%-%-----%"
+    , "%%%-%%%%%%%-%-%-%%%%%-%%%-%-%%%-%%%%%"
+    , "%-------%-%-%-%-----%---%-----%-%---%"
+    , "%%%%%-%-%-%%%%%%%%%-%%%%%%%%%%%-%-%%%"
+    , "%---%-%-----------%-%-----%---%-%---%"
+    , "%-%%%-%%%%%-%%%%%%%%%-%%%%%-%-%-%%%-%"
+    , "%-%---%------%--------%-----%-------%"
+    , "%-%-%-%%%%%-%%%-%-%-%-%-%%%%%%%%%%%%%"
+    , "%-%-%---%-----%-%-%-%-------%---%-%-%"
+    , "%-%-%%%-%%%-%-%-%-%%%%%%%%%-%%%-%-%-%"
+    , "%-%---%-%---%-%-%---%-%---%-%-%-----%"
+    , "%-%%%-%%%-%%%%%-%%%-%-%-%%%%%-%-%%%%%"
+    , "%-------%---%-----%-%-----%---%-%---%"
+    , "%%%-%-%%%%%-%%%%%-%%%-%%%-%-%%%-%-%%%"
+    , "%-%-%-%-%-%-%-%-----%-%---%-%---%-%-%"
+    , "%-%-%%%-%-%-%-%-%%%%%%%%%-%-%-%-%-%-%"
+    , "%---%---%---%-----------------%-----%"
+    , "%-%-%-%-%%%-%%%-%%%%%%%-%%%-%%%-%%%-%"
+    , "%-%.%-%-------%---%-------%---%-%---%"
+    , "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"]
  
